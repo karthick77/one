ee12275b18ced83644e3a2f271e10dd840426d95
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 12:01:40 +0530

    First Commit...

---
+++ src/test/java/Test1/Data1.java
@@ -0,0 +1,17 @@
+package Test1;
+
+public class Data1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") {
+			System.out.println("Start eating....");
+		}else {
+			System.out.println("Finish eating...");
+		}
+
+	}
+
+}
